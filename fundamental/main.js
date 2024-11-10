41fb87fc306f5990d34aa031ecbd2485570f24cb
bestchanel <samsunga77789@gmail.com>
2024-11-10 23:06:51 +0700

    Learning_Day_2

--- fundamental/main.js
+++ fundamental/main.js
@@ -7,12 +7,12 @@
 
 // คำสั่งในการแสดงผลข้อความออกไปที่หน้าเว็บ
 document.write("Hello JavaScript<br>");
-document.write("<b>ยินดีต้อนรับ</b>");
-document.write("<h1><font color='red'>ยินดีต้อนรับ</font></h1>");
+document.write("<b>ยินดีต้อนรับทุกท่าน</b>");
+document.write("<h1><font color='red'>ยินดีต้อนรับทุกท่าน</font></h1>");
 
 // การสร้างตัวแปรใน JavaScript
-var name = "Woraprat Charoenmueangphiaw";
-var age = 22;
+var name = "Samit Koyom";
+var age = 28;
 
 // แสดงผลไปที่ Console
 //console.log(name);
@@ -23,7 +23,7 @@ var age = 22;
 // console.log(status);
 
 // Method Prompt
-// var msg = prompt("กรุณาป้อนชื่อ", "วรปรัชญ์");
+// var msg = prompt("กรุณาป้อนชื่อ", "สามิตร");
 // console.log(msg);
 
 // Condition Operator
@@ -65,3 +65,5 @@ switch(day){
         document.body.style.backgroundColor = "white";
         break;
 }
+
+// Day 1 Complete
